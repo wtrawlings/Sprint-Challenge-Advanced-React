fc0c7832e47787588c13cf7fe0718b5b69748f6a
wtrawlings <rawlings.warren@gmail.com>
2019-12-16 20:30:13 -0500

    returning null on NameData

--- client/src/App.js
+++ client/src/App.js
@@ -2,40 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Axios from "axios";
-
+import NameData from "./components/NameData"
 
 class App extends React.Component {
   constructor() {
     super()
     //this is where state will go
     this.state = {
-      dataset: []
+      dataSet: []
     }
   };
   componentDidMount() {
     Axios.get("http://localhost:5000/api/players")
       .then(response => {
-        console.log(response.data)
+        //console.log(response.data)
         this.setState({dataSet: response.data})
       })
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>You can do this Warren!</h1>
+        <p>Here is some data they want displayed</p>
+        <NameData 
+          dataSet={this.state.dataSet}
+        />
       </div>
     );
   }
---
+++ client/src/components/NameData.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function NameData (props) {
+    console.log(`"from inside the NameData component" ${props}`)
+    return(
+        null
+    )
+}
+
+export default NameData;
