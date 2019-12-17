d291c935914cad74a6724ba35df443d3fb09b3d3
wtrawlings <rawlings.warren@gmail.com>
2019-12-16 20:15:20 -0500

    Axios is working

--- client/src/App.js
+++ client/src/App.js
@@ -13,30 +13,33 @@ class App extends React.Component {
     }
   };
   componentDidMount() {
-    Axios.get("")
+    Axios.get("http://localhost:5000/api/players")
       .then(response => {
         console.log(response.data)
         this.setState({dataSet: response.data})
       })
   }
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
