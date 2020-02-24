62b0e3f42edeb9333545dd23ff1b79fdf1494000
wtrawlings <rawlings.warren@gmail.com>
2020-02-23 21:04:24 -0500

    I have a toggle working

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,32 @@ import React from 'react';
 import './App.css';
 import Axios from "axios";
 import NameData from "./components/NameData"
+import useLocalStorage from "./hooks/useLocalStorage";
+
 
 class App extends React.Component {
   constructor() {
     super()
     //this is where state will go
     this.state = {
-      dataSet: []
+      dataSet: [],
+      clickToggle: false
     }
+    
+    this.toggleMode = (e, clickToggle) => {
+      e.preventDefault();
+      console.log("clicked!")
+      console.log(this.state.clickToggle) //shows up as undef
+      //console.log(this.clickToggle) //undef
+      //this.clickToggle(!clickToggle);
+      this.state.clickToggle=!this.state.clickToggle;
+      console.log(this.state.clickToggle);
+      //const [clickToggle, setClickToggle] = useLocalStorage("key", clickToggle);
+      //I can't useLocalStorage in this because it's not a component just a method
+      //inside of a component. I want to save clickToggle and use that
+      //but it's not working, the clickToggle is only changing once.
+     
+    };
   };
   componentDidMount() {
     Axios.get("http://localhost:5000/api/players")
@@ -18,11 +36,15 @@ class App extends React.Component {
         this.setState({dataSet: response.data})
       })
   }
+   
+
+
   render() {
     return (
       <div className="App">
         <h1>You can do this Warren!</h1>
         <p>Here is some data they want displayed. </p><p>Now we need to make a hook and do a couple of tests.</p>
+        <button onClick={this.toggleMode}> toggle </button>
         <NameData 
           dataSet={this.state.dataSet}
         />
