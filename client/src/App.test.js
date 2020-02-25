a4dbee87a4e6652da38a40c5190b4d634f6647ae
wtrawlings <rawlings.warren@gmail.com>
2020-02-24 20:21:14 -0500

    started testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { render } from "@testing-library/react";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+test("renders App without crashing", () => {
+  render(<App />);
 });
+
+test("verify Warren text is in app", async () => {
+  //arrange
+  const { getByText } = render(<App />);
+  //act
+  const verify1 = getByText(/warren/i)
+  //assert
+  expect(verify1).toBeInTheDocument();
+})
+
